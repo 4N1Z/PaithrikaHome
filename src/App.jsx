d02d850c35ce7e1636f786d9798b941f02ef4252
aswnss <aswnss.m@gmail.com>
2022-12-16 00:50:08 +0530

    added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./App.css"
 import Home from "./components/Home/Home"
 import Navbar from './components/Navbar/Navbar'
+import Projects from './components/Projects/Projects'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
       <Navbar/>
       <Home/>
-      <Home/>
+      <Projects/>
       
     </div>
   )
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import "./Projects.css"
+function Projects() {
+  return (
+    <div className='projects-container'>
+        <div className="project" id='upcoming-project'onClick={()=>{
+            document.getElementsByClassName('project')[0].style.width = '50%'
+            document.getElementsByClassName('project')[0].style.writingMode = ''
+            document.getElementsByClassName('project')[0].style.textOrientation = ''
+
+            document.getElementsByClassName('project')[1].style.width = '25%'
+            document.getElementsByClassName('project')[1].style.writingMode = 'vertical-rl'
+            document.getElementsByClassName('project')[1].style.textOrientation = 'upright'
+
+            document.getElementsByClassName('project')[2].style.width = '25%'
+            document.getElementsByClassName('project')[2].style.writingMode = 'vertical-rl'
+            document.getElementsByClassName('project')[2].style.textOrientation = 'upright'
+            
+        }}
+        style={
+            {
+                backgroundImage : "url('https://w0.peakpx.com/wallpaper/747/327/HD-wallpaper-tanjiro-pic-fan-art-anime-demon-slayer-kimetsu-no-yaiba.jpg')",
+                width:"50%",
+            }
+        }>
+            <p><span className='project-bold'>Upcoming</span><br />Projects</p>
+        </div>
+        <div className="project" id='ongoing-project' onClick={()=>{
+            document.getElementsByClassName('project')[1].style.width = '50%'
+            document.getElementsByClassName('project')[1].style.writingMode = ''
+            document.getElementsByClassName('project')[1].style.textOrientation = ''
+
+            document.getElementsByClassName('project')[0].style.width = '25%'
+            document.getElementsByClassName('project')[0].style.writingMode = 'vertical-rl'
+            document.getElementsByClassName('project')[0].style.textOrientation = 'upright'
+
+            document.getElementsByClassName('project')[2].style.width = '25%'
+            document.getElementsByClassName('project')[2].style.writingMode = 'vertical-rl'
+            document.getElementsByClassName('project')[2].style.textOrientation = 'upright'
+        }}
+        style={
+            {
+                backgroundImage : "url('https://www.nawpic.com/media/2020/zenitsu-nawpic-68.jpg')",
+                width:"25%",
+                writingMode:'vertical-rl',
+                textOrientation : 'upright'
+            }
+        }>
+            <p><span className='project-bold'>Ongoing</span><br />Projects</p>
+        </div>
+        <div className="project" id='finished-project' onClick={()=>{
+            document.getElementsByClassName('project')[2].style.width = '50%'
+            document.getElementsByClassName('project')[2].style.writingMode = ''
+            document.getElementsByClassName('project')[2].style.textOrientation = ''
+
+            document.getElementsByClassName('project')[1].style.width = '25%'
+            document.getElementsByClassName('project')[1].style.writingMode = 'vertical-rl'
+            document.getElementsByClassName('project')[1].style.textOrientation = 'upright'
+
+            document.getElementsByClassName('project')[0].style.width = '25%'
+            document.getElementsByClassName('project')[0].style.writingMode = 'vertical-rl'
+            document.getElementsByClassName('project')[0].style.textOrientation = 'upright'
+        }}
+        style={
+            {
+                backgroundImage : "url('https://assets.puzzlefactory.pl/puzzle/439/559/original.jpg')",
+                width:"25%",
+                writingMode:'vertical-rl',
+                textOrientation : 'upright'
+            }
+        }>
+            <p><span className='project-bold'>Finished</span><br />Projects</p>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
