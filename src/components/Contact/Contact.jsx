6075cbe0ad46ffe498ab0ad31091355913be33c3
Aniz <20am014@gmail.com>
2022-12-28 21:48:36 +0530

    neat code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./Contact.css";
+import "../../App.css";
 
 function Contact() {
   return (
     <section>
-        <div className="contactContainer">
-            <h1 className="title">Contact Us</h1>
-            <div className="contactAddress"></div>
-            <div className="contactForm">
-                <div className="name">
-                    <input type="text" required />
-                    <label>Name</label>
-                </div>
-                <div className="email">
-                    <input type="email" required />
-                    <label >Email</label>
-                </div>
-                <div className="phoneNum">
-                    <input type="number" required/>
-                    <label>Phone Number</label>
-                </div>
-                <div className="message">
-                    <textarea required></textarea>
-					<label>Message</label>
-                </div>
-            </div>
+      <div className="contactContainer">
+
+        <div className="textBox">
+          <h1 className="titles_contact">Let's Talk</h1>
+          <h4 className="subtitle_contact">Say 'hi' for enguiry</h4>
         </div>
+     
+
+        <div className="contactForms">
+          <div className="contactForm">
+        
+            <div className="name">
+              <label>Name</label>
+              <input type="text" required />
+            </div>
+            <div className="grpEP">
+              <div className="email">
+                <label>Email</label>
+                <input type="email" required />
+              </div>
+              <div className="phoneNum">
+                <label>Phone Number</label>
+                <input type="number" required />
+              </div>
+            </div>
+            <div className="message">
+              <label>Message</label>
+              <textarea required></textarea>
+            </div>
+          </div>
+          <button className="submitBtn">Send Message</button>
+        </div>
+      </div>
     </section>
-    )
+  );
 }
 
-export default Contact
+export default Contact;
