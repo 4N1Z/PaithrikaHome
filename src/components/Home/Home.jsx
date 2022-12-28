b0266591b460de14d7bb3edc41ee7cf9b26a59dd
Aniz <20am014@gmail.com>
2022-12-28 23:46:52 +0530

    Home is no more fucked. All thanks to Aniz

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,51 +4,61 @@ import { homePic } from "../../assets/images/export_file";
 
 function Home() {
   return (
-    <div className="Home">
-      <div className="mainContainer">
-
-        <div className="leftBox">
-
-          <div className="heading">
-            <div className="titles">
+    <section id="home">
+      <div className="Home">
+        <div className="mainContainer">
+          <div className="leftBox">
+            <div className="heading">
+              <div className="titles">
                 <span className="title1">Building your</span>
                 <br />
                 <span className="title2">Dream Home</span>
                 <br />
                 <span className="title3">has never been easier</span>
+              </div>
+              <p className="subTitles">
+                We offer end-to-end services with our expert team of architects,
+                designers, and engineers that can help you with every step of
+                the process.
+              </p>
             </div>
-            <p className="subTitles">
-              We offer end-to-end services with our expert team of architects,
-              designers, and engineers that can help you with every step of the
-              process.
-            </p>
+
+            <div className="counterContainer">
+              <div className="counter">
+                <h1 className="countNumber">24+</h1>
+                <h5 className="countTitle">
+                 
+                  Works <br />
+                  Completed
+                </h5>
+              </div>
+              <div className="counter">
+                <h1 className="countNumber">24+</h1>
+                <h5 className="countTitle">
+                 
+                  Works <br />
+                  Completed
+                </h5>
+              </div>
+              <div className="counter">
+                <h1 className="countNumber">24+</h1>
+                <h5 className="countTitle">
+              
+                  Works <br />
+                  Completed
+                </h5>
+              </div>
+            </div>
           </div>
 
-          <div className="counterContainer">
-            <div className="counter">
-              <h1 className="countNumber">24+</h1>
-              <h5 className="countTitle"> Works <br />Completed</h5>
-            </div>
-            <div className="counter">
-              <h1 className="countNumber">24+</h1>
-              <h5 className="countTitle"> Works <br />Completed</h5>
-            </div>
-            <div className="counter">
-              <h1 className="countNumber">24+</h1>
-              <h5 className="countTitle"> Works <br />Completed</h5>
+          <div className="rightBox">
+            <div className="Gallery">
+              <img className="homePic" src={homePic} alt="home pic" />
             </div>
           </div>
-
         </div>
-
-        <div className="rightBox">
-          <div className="Gallery">
-            <img className = "homePic" src={homePic} alt="home pic" />
-          </div>
-        </div>
-        
       </div>
-    </div>
+    </section>
   );
 }
 
