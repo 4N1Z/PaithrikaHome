b582f69ba7080111791d1024619c6597623a7591
Aniz <20am014@gmail.com>
2023-01-03 00:27:15 +0530

    One stop solutins added

--- src/components/OneStop/OneStop.jsx
+++ src/components/OneStop/OneStop.jsx
@@ -8,91 +8,30 @@ function OneStop() {
     <section id="OneStop">
       <div className="OneStopDiv">
         <img src={oneStopLogo} alt="" className="onebg" />
-        <h1 className="titleOneStop">One Stop Solution</h1>
-  
+        <h1 className="titleOneStop">Services Offered</h1>
+
         <div class="rowMain-container">
           <div class="rowHeading"></div>
           <div class="cards">
             <div class="card card-1">
               <div className="card-bg"></div>
-              <div class="card__icon">
-                <i class="fas fa-bolt"></i>
-              </div>
-              <p class="card__exit">
-                <i class="fas fa-times"></i>
-              </p>
-              <h2 class="card__title">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </h2>
-              <p class="card__apply">
-                <a class="card__link" href="#">
-                  Apply Now <i class="fas fa-arrow-right"></i>
-                </a>
-              </p>
+              <h2 class="card__title">Resale</h2>
             </div>
+
             <div class="card card-2">
-              <div class="card__icon">
-                <i class="fas fa-bolt"></i>
-              </div>
-              <p class="card__exit">
-                <i class="fas fa-times"></i>
-              </p>
-              <h2 class="card__title">
-                Sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua.
-              </h2>
-              <p class="card__apply">
-                <a class="card__link" href="#">
-                  Apply Now <i class="fas fa-arrow-right"></i>
-                </a>
-              </p>
+              <h2 class="card__title">Rental service</h2>
             </div>
+
             <div class="card card-3">
-              <div class="card__icon">
-                <i class="fas fa-bolt"></i>
-              </div>
-              <p class="card__exit">
-                <i class="fas fa-times"></i>
-              </p>
-              <h2 class="card__title">Ut enim ad minim veniam.</h2>
-              <p class="card__apply">
-                <a class="card__link" href="#">
-                  Apply Now <i class="fas fa-arrow-right"></i>
-                </a>
-              </p>
+              <h2 class="card__title">Life long maintenance</h2>
             </div>
+
             <div class="card card-4">
-              <div class="card__icon">
-                <i class="fas fa-bolt"></i>
-              </div>
-              <p class="card__exit">
-                <i class="fas fa-times"></i>
-              </p>
-              <h2 class="card__title">
-                Quis nostrud exercitation ullamco laboris nisi.
-              </h2>
-              <p class="card__apply">
-                <a class="card__link" href="#">
-                  Apply Now <i class="fas fa-arrow-right"></i>
-                </a>
-              </p>
+              <h2 class="card__title">Loan facility</h2>
             </div>
-            {/* <div class="card card-5">
-            <div class="card__icon">
-              <i class="fas fa-bolt"></i>
-            </div>
-            <p class="card__exit">
-              <i class="fas fa-times"></i>
-            </p>
-            <h2 class="card__title">
-              Ut aliquip ex ea commodo consequat. Duis aute irure dolor.
-            </h2>
-            <p class="card__apply">
-              <a class="card__link" href="#">
-                Apply Now <i class="fas fa-arrow-right"></i>
-              </a>
-            </p>
-          </div> */}
+            {/* <div class="card card-4">
+              <h2 class="card__title">Loan facility</h2>
+            </div> */}
           </div>
         </div>
       </div>
