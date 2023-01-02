5f05c43f08fa100afd2290a23f4098f168ec3778
Aniz <20am014@gmail.com>
2023-01-02 19:04:08 +0530

    Winter Season added

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,79 +1,75 @@
-
 export const navLinks = [
-    {
-      id: "home",
-      title: "Home",
-    },
-    {
-      id: "about",
-      title: "About",
-    },
-    {
-      id: "project",
-      title: "Project",
-    },
-    {
-      id: "gallery",
-      title: "Gallery",
-    },
-    {
-      id: "contact",
-      title: "Contact",
-    },
-  ];
-
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "project",
+    title: "Project",
+  },
+  {
+    id: "gallery",
+    title: "Gallery",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
 
 export const stats = [
-    {
-      id: "stats-1",
-      title: "User Active",
-      value: "3800+",
-    },
-    {
-      id: "stats-2",
-      title: "Trusted by Company",
-      value: "230+",
-    },
-    {
-      id: "stats-3",
-      title: "Transaction",
-      value: "$230M+",
-    },
-  ];
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
 
+export const project_images = {
+  ongoing:
+    "url('https://w0.peakpx.com/wallpaper/747/327/HD-wallpaper-tanjiro-pic-fan-art-anime-demon-slayer-kimetsu-no-yaiba.jpg')",
+  upcoming: "url('https://www.nawpic.com/media/2020/zenitsu-nawpic-68.jpg')",
+  finished:
+    "url('https://assets.puzzlefactory.pl/puzzle/439/559/original.jpg')",
+};
 
-
-  export const project_images = {
-    ongoing:"url('https://w0.peakpx.com/wallpaper/747/327/HD-wallpaper-tanjiro-pic-fan-art-anime-demon-slayer-kimetsu-no-yaiba.jpg')",
-    upcoming : "url('https://www.nawpic.com/media/2020/zenitsu-nawpic-68.jpg')",
-    finished : "url('https://assets.puzzlefactory.pl/puzzle/439/559/original.jpg')"
-}
-
-  export const sampleTestimonials= [
-    {
-      detail : "From my experience of taking possession of my villa at Pranamam, I can emphatically state that Paithrika is the place to go if anyone is looking to own one. Reputed, reliable and with a great tract record, Paithrika can be trusted for its commitment towards quality and timely delivery.",
-      id : "MAHESH M NAIR",
-      designation : "Joint Director ",
-      post : "STPI, Technopark"
-    },
-    {
-      detail : "Paithrika extended its dedicated team's skill and support for maintenance or modifications even after the promised support term which makes you stand out. All the best for your future endeavors and thanks for delivering our <b> customized <b/> dream home much earlier than promised, with uncompromised quality and no hidden charge.",
-      id : "Aravind Nataraja Pillai ",
-      designation : "Joint Director ",
-      post : "Allianz"
-    },
-    {
-      detail : "My professional stint in Trivandrum gave me the confidence to invest in a home away from Delhi. My search ended when I met Paithrika team. To monitor the progress of your home sitting thousands of miles away is practically impossible. They made it so very easy that I bought one more villa from them. Thank you.",
-      id: "Vineet kapoor. ",
-      designation : "Software engineer",
-      post : "Delhi"
-    },
-    
-  ]
-
-
-   
-  
+export const sampleTestimonials = [
+  {
+    detail:
+      "From my experience of taking possession of my villa at Pranamam, I can emphatically state that Paithrika is the place to go if anyone is looking to own one. Reputed, reliable and with a great tract record, Paithrika can be trusted for its commitment towards quality and timely delivery.",
+    id: "MAHESH M NAIR",
+    designation: "Joint Director ",
+    post: "STPI, Technopark",
+  },
+  {
+    detail:
+      "Paithrika extended its dedicated team's skill and support for maintenance or modifications even after the promised support term which makes you stand out. All the best for your future endeavors and thanks for delivering our <b> customized <b/> dream home much earlier than promised, with uncompromised quality and no hidden charge.",
+    id: "Aravind Nataraja Pillai ",
+    designation: "Joint Director ",
+    post: "Allianz",
+  },
+  {
+    detail:
+      "My professional stint in Trivandrum gave me the confidence to invest in a home away from Delhi. My search ended when I met Paithrika team. To monitor the progress of your home sitting thousands of miles away is practically impossible. They made it so very easy that I bought one more villa from them. Thank you.",
+    id: "Vineet kapoor. ",
+    designation: "Software engineer",
+    post: "Delhi",
+  },
+];
 
 //   export const socialMedia = [
 //     {
