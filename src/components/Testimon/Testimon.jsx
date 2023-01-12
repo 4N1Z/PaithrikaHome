4fe9a559a93ba176e11ec84aff007c879898eb27
aswnss <aswnss.m@gmail.com>
2023-01-12 16:38:16 +0530

    working on horizontal testimonials

---
+++ src/components/Testimon/Testimon.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Testimon.css"
+
+function Testimon() {
+  return (
+    <div className='testimonial-container'>
+          <ul class="hs full no-scrollbar">
+    <li class="item">test</li>
+    <li class="item">test</li>
+    <li class="item">test</li>
+    <li class="item">test</li>
+    <li class="item">test</li>
+    <li class="item">test</li>
+  </ul>
+    </div>
+  )
+}
+
+export default Testimon
