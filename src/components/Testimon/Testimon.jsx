660a4d9d4effba0789341bb6283d909b04cbc086
aswnss <aswnss.m@gmail.com>
2023-01-12 17:04:08 +0530

    scrolling testimonial without design

--- src/components/Testimon/Testimon.jsx
+++ src/components/Testimon/Testimon.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import "./Testimon.css"
+import { sampleTestimonials } from "../../constants/index.js";
+import { quotes, quotes2 } from "../../assets/images/export_file";
 
 function Testimon() {
   return (
     <div className='testimonial-container'>
-          <ul class="hs full no-scrollbar">
-    <li class="item">test</li>
-    <li class="item">test</li>
-    <li class="item">test</li>
-    <li class="item">test</li>
-    <li class="item">test</li>
-    <li class="item">test</li>
-  </ul>
+      <ul class="hs full no-scrollbar">
+       {sampleTestimonials.map((item)=>(
+        <li className="item">
+          <p className="review">
+            {item.detail}
+          </p>
+          <p className="user">
+            {item.id} <br />
+            {item.designation} <br />
+            {item.post}
+          </p>
+        </li>
+       ))}
+      </ul>
     </div>
   )
 }
