64b2931c4bad98718552cae1fc87570f68a124d8
Aniz <20am014@gmail.com>
2023-01-13 21:34:29 +0530

    testmonial bug fixed

--- src/components/Testimon/Testimon.jsx
+++ src/components/Testimon/Testimon.jsx
@@ -1,40 +1,39 @@
-import React from 'react'
-import "./Testimon.css"
+import React from "react";
+import "./Testimon.css";
 import { sampleTestimonials } from "../../constants/index.js";
 import { quotes, quotes2 } from "../../assets/images/export_file";
 
 function Testimon() {
   return (
-
-    <div className='testimonial-container'>
-      <h1 className='titleTestimon'>Testimonials</h1>
+    <div className="testimonial-container">
+      <h1 className="titleTestimon">Testimonials</h1>
 
       <div className="content-container">
         <ul class="hs full no-scrollbar">
-          
-          {/*  */}
-        {sampleTestimonials.map((item)=>(
-          <div className='testimonial-bar'>
-          <li className="item">
-            <div className="quotes-Testimonial">
-            <img className="quoteImage" src={quotes} alt="" />
 
-            <p className="review">
-              {item.detail}
-            </p>
-            <br /></div>
-            <p className="user">
-              <span className='user-name'>{item.id}</span> <br />
-              {item.designation} <br />
-              {item.post}
-            </p>
-          </li>
-          </div>
-        ))}
+          {sampleTestimonials.map((item) => (
+            <div className="testimonial-bar">
+              <li className="item">
+                <div className="quotes-Testimonial">
+                  <img className="quoteImage" src={quotes} alt="" />
+
+                  <p className="review">{item.detail}</p>
+                </div>
+                  <br />
+                <p className="user">
+                {item.id}
+                  <span className="user-name"><br />
+                  {item.designation} <br />
+                  {item.post}
+                  </span> 
+                </p>
+              </li>
+            </div>
+          ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Testimon
+export default Testimon;
